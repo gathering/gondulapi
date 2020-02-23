316e88ef432a997cf3a628ba248b56a4af4f6f88
Kristian Lyngstol <kly@kly.no>
2020-02-23 13:38:37 +0100

    Start cleaning the emerging core API

--- main.go
+++ main.go
@@ -3,45 +3,64 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/url"
 	"io"
 	"net/http"
+	"net/url"
 
 	log "github.com/sirupsen/logrus"
 )
 
-
-type receiver struct {}
-
 type Input struct {
-	Method	string
-	Public	bool
-	Data	[]byte
-	URL	*url.URL
+	Method string
+	Public bool
+	Data   []byte
+	URL    *url.URL
 }
 
 type Output struct {
-	Data interface{}
-	Failed bool
+	Data         interface{}
+	Failed       bool
 	CacheControl string
 }
+
+type allocator func() interface{}
+
+type receiver struct {
+	alloc allocator
+	path  string
+}
+
+type Getter interface {
+	Get(element string) error
+}
+type Putter interface {
+	Put(element string) (error)
+}
+type Poster interface {
+	Post() (error)
+}
+type Deleter interface {
+	Delete(element string) (error)
+}
+
+var handles map[string]allocator
 
 func (rcvr receiver) answer(w http.ResponseWriter, output Output, pretty bool) {
 	var b []byte
 	var err error
 	if pretty {
-		b, err = json.MarshalIndent(output.Data,"","  ")
+		b, err = json.MarshalIndent(output.Data, "", "  ")
 	} else {
 		b, err = json.Marshal(output.Data)
 	}
 	code := 200
-	if output.Failed  || err != nil{
+	if output.Failed || err != nil {
 		code = 400
 	}
 	if err != nil {
 		log.Printf("Got error? %v", err)
 	}
-	w.Header().Set("ETag","KEK")
+	w.Header().Set("ETag", "KEK")
 	w.WriteHeader(code)
 	if code == 204 {
 		return
@@ -67,97 +86,68 @@ func (rcvr receiver) get(w http.ResponseWriter, r *http.Request) (Input, error) 
 		}
 	}
 
-
 	return d, nil
 }
 
-
-var enBox box
-
-
-
-
-
-
-
-type box struct {
-	Sysname string
-}
-
-func (b *box) Get(element string) (Output, error) {
-	var o Output
-	o.Data = enBox
-	return o,nil	
-}
-
-func (b box) Put(element string) (Output, error) {
-	enBox = b
-	fmt.Printf("fff: %v\n",enBox)
-	var o Output
-	o.Data = "saaaved"
-	return o,nil
-}
-
-
-
-type handler interface{}
-
-type Getter interface {
-	Get(element string) (Output, error)
-}
-type Putter interface {
-	Put(element string) (Output, error)
-}
-type Poster interface {
-	Post() (Output, error)
-}
-
-var handles map[string]handler
-
-
-func (i Input) handleError(err error) (output Output) {
-	output.Failed = true
-	output.Data = struct {
-		Message string
-		Error string
-	}{
-		Message: "Input error",
-		Error: err.Error(),
+func AddHandler(url string, a allocator) {
+	if handles == nil {
+		handles = make(map[string]allocator)
 	}
-	log.Printf("%s %v: Failed to parse: %v", i.Method, i.URL, err)
-	return output
+	handles[url] = a
 }
 
 func (rcvr receiver) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	input, err := rcvr.get(w, r)
-	log.Debugf("got %s - err %v",input.Data,err)
-	pretty := len(input.URL.Query()["pretty"])>0
+	log.Debugf("got %s - err %v", input.Data, err)
+	pretty := len(input.URL.Query()["pretty"]) > 0
 	var output Output
-	if input.URL.Path[0:len("/switch/")] == "/switch/" {
-		fmt.Printf("hei")
-			b := box{}
-		if input.Method == "GET" {
-			output,_ = b.Get(input.URL.Path[len("/switch"):])
-		} else if input.Method == "PUT" {
-			fmt.Printf("HEI")
-		     err := json.Unmarshal(input.Data,&b)
-		     if err != nil {
-			     output.Failed = true
-		     }
-			output,_ = b.Put(input.URL.Path[len("/switch"):])
+	item := rcvr.alloc()
+	if input.Method == "GET" {
+		get := item.(Getter)
+		err = get.Get(input.URL.Path[len(rcvr.path):])
+		if err == nil {
+			output.Data = get
+		} else {
+			output.Failed = true
+			output.Data = struct{Message string;Error string}{Message: "Getting element failed",Error: fmt.Sprintf("%v",err)}
 		}
-	} else {
-		fmt.Printf("kek: %v", input.URL.Path)
+	} else if input.Method == "PUT" {
+		err := json.Unmarshal(input.Data, &item)
+		if err != nil {
+			output.Failed = true
+			output.Data = struct{Message string;Error string}{Message: "Saving failed",Error: fmt.Sprintf("%v",err)}
+		} else {
+			put := item.(Putter)
+			err = put.Put(input.URL.Path[len(rcvr.path):])
+			if err != nil {
+				output.Failed = true
+				output.Data = struct{Message string;Error string}{Message: "Saving element failed",Error: fmt.Sprintf("%v",err)}
+			} else {
+				output.Data = struct{Message string}{Message: "Item stored"}
+			}
+		}
+	} else if input.Method == "DELETE" {
+		del := item.(Deleter)
+		err = del.Delete(input.URL.Path[len(rcvr.path):])
+		if err == nil {
+			output.Data = struct{Message string}{Message: "Deleted"}
+		} else {
+			output.Failed = true
+			output.Data = struct{Message string;Error string}{Message: "Deleting of element failed",Error: fmt.Sprintf("%v",err)}
+		}
 	}
-	rcvr.answer(w, output,pretty)
+	rcvr.answer(w, output, pretty)
 }
 
 func main() {
 	server := http.Server{}
 	serveMux := http.NewServeMux()
 	server.Handler = serveMux
-	serveMux.Handle("/", receiver{})
-	server.Addr =  "[::1]:8080"
+	for idx, h := range handles {
+		log.Printf("idx: %v h: %v\n", idx, h)
+		serveMux.Handle(idx, receiver{alloc: h, path: idx})
+	}
+	server.Addr = "[::1]:8080"
 	log.WithField("address", server.Addr).Info("Starting http receiver")
 	log.Fatal(server.ListenAndServe())
 }
